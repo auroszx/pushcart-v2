80c88921fc729c0c3916019e877870c16485e03d
Andrés Hernández <andres.hernandez@emqu.net>
2018-11-26 13:40:51 -0400

    Comments endpoint fixes

--- controllers/CommentsController.js
+++ controllers/CommentsController.js
@@ -8,7 +8,7 @@ module.exports = {
 	getCommentsByProduct: async function(product_id, token) {
 		if (token != undefined) {
 			db.connect();
-			var comments = await db.get("SELECT product_comment_id, product_comments.user_id, product_comment FROM product_comments JOIN users ON users.user_id = product_comments.user_id WHERE product_id = (?)", [product_id]);
+			var comments = await db.get("SELECT product_comment_id, product_comments.user_id, product_comment, users.user_fullname, users.user_username FROM product_comments JOIN users ON users.user_id = product_comments.user_id WHERE product_id = (?)", [product_id]);
 			return { status: 200, comments: comments };
 		}
 		else {
@@ -21,14 +21,14 @@ module.exports = {
 			var data = await auth.verify(token);
 			db.connect();
 			var id = await db.execute("INSERT INTO product_comments (product_id, user_id, product_comment) VALUES ((?), (?), (?))", [product_id, data.user_id, product_comment]);
-			return {status: 200, product: this.getProductInfo(id, token) };
+			return {status: 200, product: this.getCommentById(id, token) };
 		}
 	},
 
 	getCommentById: async function(product_comment_id, token) {
 		if (token != undefined) {
 			db.connect();
-			return db.get("SELECT product_comment_id, product_comments.user_id, product_comment FROM product_comments JOIN users ON users.user_id = product_comments.user_id WHERE product_id = (?)", [product_comment_id]);
+			return {status: 200, comments: await db.get("SELECT product_comment_id, product_comments.user_id, product_comment, users.user_fullname, users.user_username FROM product_comments JOIN users ON users.user_id = product_comments.user_id WHERE product_comment_id = (?)", [product_comment_id]);
 		}
 		else {
 			return { status: 403, message: "You are not allowed to perform this action" };
